2d4af2eddfcb445938fa6780d7cd439d99b56af6
Ashay Sandesh Saoji Personal <ashayssaoji@gmail.com>
2024-05-24 19:54:21 -0400

    Delet products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -103,7 +103,9 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
     console.log(cart);
-   
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   });
 });
 
